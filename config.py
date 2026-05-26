419abe91177951e14454feb5ffee54ab204540a6
lxndrbnsv <abnosov@gmail.com>
2026-05-26 13:43:53 +0200

    Upd

--- config.py
+++
@@ -1,7 +0,0 @@
-class Config(object):
-    # --- --- --- DATABASE --- --- --- --- --- --- --- --- --- --- --- --- ---
-    DATABASE_NAME = "cir"
-    DATABASE_USER = "db_user"
-    DATABASE_PASSWORD = "v9OJF3KKbD1yj6Px"
-    DATABASE_HOST = "45.146.165.125"
-    DATABASE_PORT = "3306"
